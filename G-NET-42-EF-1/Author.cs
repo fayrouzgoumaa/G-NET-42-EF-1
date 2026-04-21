d39e39aa5ebbccc707ba2d3f9227e2a24f79b7e3
fayro <fayro@FAYROUZGOUMAA>
2026-04-22 01:40:12 +0200

    class Author

--- G-NET-42-EF-1/Author.cs
+++ G-NET-42-EF-1/Author.cs
@@ -8,6 +8,12 @@ namespace G_NET_42_EF_1
 {
     internal class Author
     {
-        public 
+        public int Id { get; set; }
+        public string First { get; set; } 
+       public  string LastName {  get; set; }
+        public string Email {  get; set; }
+        public string Biography {  get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public List<Book> Books {  get; set; }=new List<Book>();
     }
 }
