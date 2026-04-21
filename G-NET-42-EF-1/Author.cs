461cda8871d79ea7e191922da5ba3dc60290b8cd
fayro <fayro@FAYROUZGOUMAA>
2026-04-22 01:35:02 +0200

    book class

---
+++ G-NET-42-EF-1/Author.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_42_EF_1
+{
+    internal class Author
+    {
+        public 
+    }
+}
---
+++ G-NET-42-EF-1/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_42_EF_1
+{
+    internal class Book
+    {
+        public int Id { get; set; }
+        public string ISBN { get; set; }
+        public double price { get; set; }
+        public int NumberOfPages { get; set; }
+        public int YearPublished { get; set; }
+        public bool IsInStock { get; set; }
+        public int categoryId {  get; set; }
+        public category category {  get; set; }
+        public List<Author> Authors { get; set; } = new List<Author>();
+
+    }
+}
