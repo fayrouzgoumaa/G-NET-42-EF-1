7cee7d676902cb2fd43536b8c72f9e6b4315f30d
fayro <fayro@FAYROUZGOUMAA>
2026-04-22 01:44:22 +0200

    class category

---
+++ G-NET-42-EF-1/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_42_EF_1
+{
+    internal class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+        public List<Book>Books { get; set; }=new List<Book>();
+
+    }
+}
